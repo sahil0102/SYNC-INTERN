2695e3e4ea1979d8bc5fdd7ea2211047a7d421c5
sahil0102 <132296606+sahil0102@users.noreply.github.com>
2023-05-02 00:05:22 +0530

    Add files via upload

---
+++ SY-03/quiz.js
@@ -0,0 +1,118 @@
+let quiz_rules = [
+    "You Have Only 20 Seconds For Each Question.",
+    "Once You Select Any Answer.It Can't Be Undone",
+    "You'll Get Points On The Basic Of Your Correct Answers."
+];
+
+
+let quiz_questions = [
+    // question 1
+    {
+        // question
+        question: "What does HTML stand for?",
+
+        // options
+        options : [
+            "Hyper Text Markup Language",
+            "Hyperlinks and Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyper Text Making Language",
+        ],
+
+        // correct answer
+        answer: 1,
+    },
+    
+    {
+        // write yout question here
+        question: "What does CSS stand for?",
+    
+        options: [
+            "Computer Style Sheet",
+            "Cascading Style Sheets",
+            "Colorful Style Sheets",
+            "Createive Style Sheets",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Inside which HTML element do we put the JavaScript?",
+    
+        options: [
+            "script",
+            "scripting",
+            "js",
+            "javascript",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "What does SQL stand for?",
+    
+        options: [
+            "Structure Query Language",
+            "Strong Question Language",
+            "Structured Question Language",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "What does PHP stand for?",
+    
+        options: [
+            "PHP: Hypertext Preprocessor",
+            "Personal Hypertext Processor",
+            "Private Homepage",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Which of the following is correct?",
+    
+        options: [
+            "JQuery is a JavaScript Library",
+            "JQuery is a JSON Library",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Bootstrap 3 is mobile-first.",
+    
+        options: [
+            "False",
+            "True",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "What is a correct syntax to output 'Hello World' in C++?",
+    
+        options: [
+            'System.out.printLn("Hello World")',
+            'cout << "Hello World"',
+            'print("Hello World")',
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    
+];
